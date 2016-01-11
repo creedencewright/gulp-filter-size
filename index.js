57546ae8f1ac2158c93c417beaf3b401d1b90d8d
Denis Smirnov <creedencewright@gmail.com>
2016-01-11 22:32:17 +0600

    added min size

--- index.js
+++ index.js
@@ -3,8 +3,20 @@
 var through       = require('through2');
 var StreamCounter = require('stream-counter');
 
+var DEFAULT_MIN_SIZE = 0;
+var DEFAULT_MAX_SIZE = 1000;
+
 module.exports = function(sizeLimit) {
-    sizeLimit = sizeLimit || 1000;
+    var sizeMin, sizeMax;
+
+    if (typeof sizeLimit === 'number') {
+        sizeMax = sizeLimit;
+    } else if (typeof sizeLimit === 'object') {
+        sizeMin = sizeLimit.min || DEFAULT_MIN_SIZE;
+        sizeMax = sizeLimit.max || DEFAULT_MAX_SIZE;
+    } else {
+        sizeMax = DEFAULT_MAX_SIZE;
+    }
 
     return through.obj(function(file, encoding, next) {
         if (file.isNull()) {
@@ -15,12 +27,16 @@ module.exports = function(sizeLimit) {
             file.contents.pipe(new StreamCounter())
                 .on('error', function() {next(null)})
                 .on('finish', function() {
-                    return this.bytes <= sizeLimit ? next(null, file) : next(null);
+                    return _check(sizeMin, sizeMax, this.bytes) ? next(null, file) : next(null);
                 });
 
             return;
         }
 
-        return file.contents.length <= sizeLimit ? next(null, file) : next(null);
+        return _check(sizeMin, sizeMax, file.contents.length) ? next(null, file) : next(null);
     });
 };
+
+function _check(min, max, size) {
+    return size >= min && size <= max;
+}
